7a32127bb33c48a7fb9756658fca867efc14aeeb
LeeJueon <loolloo080313@gmail.com>
2024-11-22 12:20:07 +0900

    refactor: 코드 main으로 일부 옮김

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:jovajovajova/screens/notification_screen.dart';
 import 'package:jovajovajova/widget/top_container.dart';
-import 'package:jovajovajova/widget/bottom_buttons.dart';
 import 'package:jovajovajova/widget/bestwriting_container.dart';
-import 'package:jovajovajova/state/nagivator/button_click.dart';
+import 'joboffer_screen.dart';
+import 'jobsearch_screen.dart';
+import 'myprofile_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -23,49 +25,28 @@ class _MainScreenState extends State<MainScreen> {
     });
   }
 
+  var _index = 2;
+
+  List<Widget> _pages = [
+    JobofferScreen(),
+    JobsearchScreen(),
+    MainScreen(),
+    MyprofileScreen(),
+    NotificationScreen()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey[300],
-        body: Column(
+        body:  const Column(
           children: [
-            const TopContainer(),
-            const BestwritingContainer(title: "조회수가 가장 많은 구인", body: "이런 친구를 굉장히 좋아합니다람쥐ㅇㅇㅇ",post_title: "플러터 개발자",),
-            const SizedBox(height: 30),
-            const BestwritingContainer(title: "조회수가 가장 많은 구직", body: "하",post_title: "롤로노아 김동현 팀",),
-            Flexible(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  BottomButtons(
-                      icon: const Icon(Icons.person_search),
-                      color: false,
-                      click: (context) {
-                        ButtonActions.jobOfferButton(context);
-                        ButtonClickState(jobofferclick);
-                        }
-                      ),
-                  BottomButtons(
-                      icon: const Icon(Icons.assignment),
-                      color: false,
-                      click: (context) {
-                        ButtonActions.jobSerachButton(context);
-                        ButtonClickState(jobsearchclick);
-                      }
-                  ),
-                  BottomButtons(
-                      icon: const Icon(Icons.person),
-                      color: false,
-                      click: (context) {
-                        ButtonActions.myprofileButton(context);
-                      }
-                  ),
-                ],
-              ),
-            ),
+            TopContainer(),
+            BestwritingContainer(title: "조회수가 가장 많은 구인", body: "이런 친구를 굉장히 좋아합니다람쥐ㅇㅇㅇ",post_title: "플러터 개발자",),
+            SizedBox(height: 30),
+            BestwritingContainer(title: "조회수가 가장 많은 구직", body: "하",post_title: "롤로노아 김동현 팀",),
           ],
         ),
       ),
