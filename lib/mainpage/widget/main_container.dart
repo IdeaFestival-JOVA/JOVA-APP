1f80fc226997dc75f929018394c796811b337b2f
LeeJueon <loolloo080313@gmail.com>
2024-12-03 16:57:49 +0900

    refactor: 글씨 크기 조정

--- lib/mainpage/widget/main_container.dart
+++ lib/mainpage/widget/main_container.dart
@@ -34,48 +34,51 @@ class MainContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w700,
-            color: Colors.black
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w700,
+              color: Colors.black
+            ),
           ),
-        ),
-        const SizedBox(height: 20,),
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.grey[100],
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 1,
-                    blurRadius: 3,
-                  ),
-                ]
-              ),
-              child: ListView.separated(
-                itemBuilder: (BuildContext context, int index) {
-                  return ListPostWidget(
-                    author: author_list[index],
-                    title: title_list[index],
-                    deadline: deadline_list[index],
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) {
-                  return Divider();
-                },
-                itemCount: author_list.length,
-              ),
-          ),
-        )
-      ],
+          const SizedBox(height: 20,),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey[100],
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 3,
+                    ),
+                  ]
+                ),
+                child: ListView.separated(
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListPostWidget(
+                      author: author_list[index],
+                      title: title_list[index],
+                      deadline: deadline_list[index],
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Divider();
+                  },
+                  itemCount: author_list.length,
+                ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
