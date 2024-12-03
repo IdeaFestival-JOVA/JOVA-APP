dafb503fff243b7baff995b20ae177738054de31
LeeJueon <loolloo080313@gmail.com>
2024-12-03 09:31:00 +0900

    refactor: 리스트 완성

--- lib/mainpage/widget/main_container.dart
+++ lib/mainpage/widget/main_container.dart
@@ -10,10 +10,27 @@ class MainContainer extends StatelessWidget {
     required this.title,
   });
 
-  List<String> list = [
-    "앙",
-
+  List<String> author_list = [
+    "김동현",
+    "최준영",
+    "이상혁",
+    "이주언",
   ];
+
+   List<String> title_list = [
+     "Iot",
+     "Design",
+     "Back_End",
+     "Flutter",
+   ];
+
+   List<String> deadline_list = [
+     "2024.1.1",
+     "2024.3.13",
+     "2024.5.5",
+     "2024.7.29",
+     "2024.12.25"
+   ];
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +48,7 @@ class MainContainer extends StatelessWidget {
         const SizedBox(height: 20,),
         Expanded(
           child: Container(
-            padding: EdgeInsets.all(8),
+            padding: const EdgeInsets.symmetric(horizontal: 8),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
                 color: Colors.grey[100],
@@ -43,14 +60,18 @@ class MainContainer extends StatelessWidget {
                   ),
                 ]
               ),
-              child: ListView(
-                children: [
-                  ListPostWidget(
-                      author: "이주언",
-                      title: "디자인 모집",
-                      deadline: "2024.12.25",
-                  ),
-                ],
+              child: ListView.separated(
+                itemBuilder: (BuildContext context, int index) {
+                  return ListPostWidget(
+                    author: author_list[index],
+                    title: title_list[index],
+                    deadline: deadline_list[index],
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Divider();
+                },
+                itemCount: author_list.length,
               ),
           ),
         )
