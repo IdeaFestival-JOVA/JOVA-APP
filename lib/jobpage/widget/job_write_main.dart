cfa1e7e018c1ee73f3367548aa26b7564e598800
LeeJueon <loolloo080313@gmail.com>
2024-12-04 18:20:11 +0900

    refactor: text 파라미터 추가

--- lib/jobpage/widget/job_write_main.dart
+++ lib/jobpage/widget/job_write_main.dart
@@ -5,18 +5,19 @@ class JobWriteMain extends StatelessWidget {
   JobWriteMain({
     super.key,
     required this.controller,
-    required this.maxline,
+    required this.maxline, required this.text,
   });
 
   TextEditingController controller = TextEditingController();
   final int maxline;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       maxLines: maxline,
       decoration: InputDecoration(
-        hintText: "내용",
+        hintText: text,
         border: OutlineInputBorder(),
       ),
       controller: controller,
