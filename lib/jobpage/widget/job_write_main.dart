8d127fe5c5920329615460e70c148dae91275fa8
LeeJueon <loolloo080313@gmail.com>
2024-12-04 16:19:46 +0900

    refactor: 위젯 분리후 통합

--- lib/jobpage/widget/job_write_main.dart
+++ lib/jobpage/widget/job_write_main.dart
@@ -2,16 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class JobWriteMain extends StatelessWidget {
-  const JobWriteMain({super.key});
+  JobWriteMain({
+    super.key,
+    required this.controller,
+    required this.maxline,
+  });
+
+  TextEditingController controller = TextEditingController();
+  final int maxline;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      maxLines: 15,
+      maxLines: maxline,
       decoration: InputDecoration(
         hintText: "내용",
         border: OutlineInputBorder(),
       ),
+      controller: controller,
     );
   }
 }
