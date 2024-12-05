3a3c894ea082626ebd29488806647354b7f1b923
LeeJueon <loolloo080313@gmail.com>
2024-12-05 14:38:00 +0900

    refactor: 파라미터 값 추가

--- lib/jobpage/screen/job_screen.dart
+++ lib/jobpage/screen/job_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:jovajovajova/jobpage/screen/job_write_screen.dart';
 import 'package:jovajovajova/jobpage/widget/job_list_post.dart';
 import 'package:jovajovajova/mainpage/widget/main_appbar.dart';
 
 class JobScreen extends StatefulWidget {
-  const JobScreen({super.key});
+  const JobScreen({
+    super.key,
+    this.title,
+    this.author,
+    this.deadline,
+    this.day
+  });
+
+  final TextEditingController? title;
+  final String? author;
+  final String? deadline;
+  final int? day;
 
   @override
   State<JobScreen> createState() => _JobScreenState();
@@ -14,7 +24,25 @@ class JobScreen extends StatefulWidget {
 
 class _JobScreenState extends State<JobScreen> {
 
-  List<int> day_list = [1,2,3,4,5,6,7,8];
+  void addDate(){
+    setState(() {
+      if(widget.day != null){
+        day_list.add(widget.day!);
+      }
+      if(widget.title != null){
+        title_list.add(widget.title!.text);
+      }
+      if(widget.author != null){
+        author_list.add(widget.author!);
+      }
+      if(widget.deadline != null){
+        deadline_list.add(widget.deadline!);
+      }
+    });
+  }
+
+
+  List<int?> day_list = [1,2,3,4,5,];
 
   List<String> title_list = [
     "Iot",
@@ -22,9 +50,6 @@ class _JobScreenState extends State<JobScreen> {
     "Back_End",
     "Front_End",
     "Flutter",
-    "ㅇㅇㅇㅇㅇ",
-    "ㅇㅇㅇㅇ",
-    "ㅇㅇㅇㅇ"
   ];
 
 
@@ -34,9 +59,6 @@ class _JobScreenState extends State<JobScreen> {
     "황지훈",
     "이상혁",
     "이주언",
-    "ㅇㅇㅇ",
-    "ㅇㅇㅇㅇ",
-    "ㅇㅇㅇㅇ",
   ];
 
   List<String> deadline_list = [
@@ -46,10 +68,13 @@ class _JobScreenState extends State<JobScreen> {
     "2024.4.4",
     "2024.7.29",
     "2024.12.25",
-    "14114",
-    "141434",
-    "4134134"
   ];
+
+  @override
+  void initState(){
+    super.initState();
+    addDate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +91,7 @@ class _JobScreenState extends State<JobScreen> {
           body: ListView.separated(
                   itemBuilder: (BuildContext context,int index){
                     return JobListPost(
-                      day: day_list[index],
+                      day: day_list[index]!,
                       title: title_list[index],
                       author: author_list[index],
                       deadline: deadline_list[index],
