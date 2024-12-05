c64ce721b0fc8575ebfdcf775772ba2495799305
LeeJueon <loolloo080313@gmail.com>
2024-12-05 20:10:03 +0900

    feat: 토큰 유효 조건 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:jovajovajova/screens/job_screen.dart';
-import 'package:jovajovajova/screens/job_screen.dart';
-import 'package:jovajovajova/screens/main_screen.dart';
-import 'package:jovajovajova/screens/myprofile_screen.dart';
+import 'package:jovajovajova/jobpage/screen/job_screen.dart';
+import 'package:jovajovajova/login_screen.dart';
+import 'package:jovajovajova/mainpage/screen/main_screen.dart';
+import 'package:jovajovajova/myprofile_page/screen/myprofile_screen.dart';
 import 'package:jovajovajova/screens/notification_screen.dart';
-import 'package:jovajovajova/widget/bestwriting_container.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main(){
-  runApp(const Main());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? accessToken = prefs.getString('accessToken');
+
+  runApp(MyApp(accessToken: accessToken));
 }
 
+class MyApp extends StatelessWidget {
+  final String? accessToken;
 
-
-class Main extends StatefulWidget {
-  const Main({super.key});
-
-  @override
-  State<Main> createState() => _Main();
-}
-
-class _Main extends State<Main> {
-
-  bool jobofferclick = false;
-  bool jobsearchclick = false;
-  bool myprofileclick = false;
-
-  void ButtonClickState(bool onclick){
-    setState(() {
-      onclick = !onclick;
-    });
-  }
-
-  var _index = 2;
-
-  final List<Widget> _pages = [
-    JobScreen(),
-    MainScreen(),
-    MyprofileScreen(),
-    NotificationScreen()
-  ];
+  const MyApp({Key? key, this.accessToken}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          backgroundColor: Colors.white,
-          body: _pages[_index],
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: _index,
-            onTap: (value) {
-              setState(() {
-                _index = value;
-              });
-            },
-            items:const [
-              BottomNavigationBarItem(icon: Icon(Icons.home),label: ""),
-              BottomNavigationBarItem(icon: Icon(Icons.explore),label: ""),
-              BottomNavigationBarItem(icon: Icon(Icons.campaign),label: ""),
-              BottomNavigationBarItem(icon: Icon(Icons.person),label: ""),
-            ],
-            selectedItemColor: Colors.black,
-            unselectedItemColor: Colors.white,
-          )
+      initialRoute: accessToken != null ? '/home' : '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => Main(),
+      },
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+    );
+  }
+}
+
+class Main extends StatefulWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  int _index = 0;
+
+  final List<Widget> _pages = [
+    MainScreen(),
+    JobScreen(),
+    MyprofileScreen(),
+    NotificationScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        currentIndex: _index,
+        onTap: (value) {
+          setState(() {
+            _index = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "홈"),
+          BottomNavigationBarItem(icon: Icon(Icons.explore), label: "구인구직"),
+          BottomNavigationBarItem(icon: Icon(Icons.campaign), label: "공지"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "마이페이지"),
+        ],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
       ),
     );
   }
