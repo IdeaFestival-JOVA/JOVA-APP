f8156695b8ba510ef098cf3f96680253b80944e6
LeeJueon <loolloo080313@gmail.com>
2024-12-09 23:30:52 +0900

    feat: UserModel 추가

---
+++ lib/model/user_model.dart
@@ -0,0 +1,14 @@
+class UserModel{
+  
+  final int day;
+  final String title;
+  final String author;
+  final String deadline;
+  
+  UserModel({
+    required this.day,
+    required this.title,
+    required this.author,
+    required this.deadline
+  });
+}
