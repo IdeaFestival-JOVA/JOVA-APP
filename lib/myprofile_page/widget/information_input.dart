82726e4a777049847e3a92be43d1cc283163f9ad
LeeJueon <loolloo080313@gmail.com>
2024-12-18 23:42:50 +0900

    refactor: 정보 입력 TextField 파일화

---
+++ lib/myprofile_page/widget/information_input.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InformationInput extends StatelessWidget {
+  const InformationInput({super.key, required this.name, required this.essential});
+
+  final String name;
+  final bool essential;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: name,
+                style: TextStyle(
+                  color: Colors.black,
+                )
+              ),
+              TextSpan(
+                text: essential ? " *":"",
+                style: TextStyle(
+                  color: Colors.red,
+                )
+              )
+            ]
+          ),
+        ),
+        SizedBox(height: 8,),
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        SizedBox(height: 10,),
+      ],
+    );
+  }
+}
