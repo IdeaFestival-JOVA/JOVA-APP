ae0da7a7cc84045b38b7e09e1d05fab0dbc75ffb
LeeJueon <loolloo080313@gmail.com>
2024-12-18 23:42:13 +0900

    refactor: appbar 와 body의 구분선 설정

--- lib/myprofile_page/screen/profile_information_modify.dart
+++ lib/myprofile_page/screen/profile_information_modify.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jovajovajova/myprofile_page/widget/information_input.dart';
 
 class ProfileInformationModify extends StatelessWidget {
   const ProfileInformationModify({super.key});
@@ -8,6 +9,12 @@ class ProfileInformationModify extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        shape: Border(
+          bottom: BorderSide(
+            color: Colors.black,
+            width: 0.5
+          )
+        ),
         backgroundColor: Colors.white,
         leading: IconButton(
           onPressed: () {
@@ -20,6 +27,23 @@ class ProfileInformationModify extends StatelessWidget {
           style: TextStyle(fontSize: 24),
         ),
         centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+                'asset/image/profile.png',
+                width: 100,
+              height: 100,
+            ),
+            SizedBox(height: 20,),
+            InformationInput(name: "이름",essential: true,),
+            InformationInput(name: "깃허브",essential: false,),
+            InformationInput(name: "E-mail",essential: true,),
+          ],
+        ),
       ),
     );
   }
