2dff4ca71797e0c332f0054efda233fa3ca97b03
LeeJueon <loolloo080313@gmail.com>
2024-12-23 23:10:02 +0900

    refactoring: 전공 선택하는 창 뜨게 하는 버튼

--- lib/myprofile_page/screen/profile_information_modify.dart
+++ lib/myprofile_page/screen/profile_information_modify.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jovajovajova/myprofile_page/widget/information_input.dart';
+import 'package:jovajovajova/myprofile_page/widget/major_confirm_button.dart';
 import 'package:jovajovajova/myprofile_page/widget/major_select_container.dart';
 
 class ProfileInformationModify extends StatelessWidget {
@@ -45,50 +46,7 @@ class ProfileInformationModify extends StatelessWidget {
             InformationInput(name: "깃허브",essential: false,),
             InformationInput(name: "E-mail",essential: true,),
             SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "전공",
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                SizedBox(width: 20,),
-                OutlinedButton(
-                    style: ButtonStyle(
-                      shape: WidgetStatePropertyAll(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                      ),
-                    ),
-                    onPressed: (){
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context){
-                            return AlertDialog(
-                              title: Text("전공 선택"),
-                              content: Column(
-                                children: [
-                                  MajorSelectContainer(title: "일반 전공")
-                                ],
-                              ),
-                            );
-                          },
-                      );
-                    },
-                    child: Text(
-                      "전공 선택",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                ),
-              ],
-            )
+            MajorConfirmButton(),
           ],
         ),
       ),
