a8ff8831661c86ce4cc1377b0570be8cf1a7e9f4
LeeJueon <loolloo080313@gmail.com>
2024-12-26 09:04:01 +0900

    feat: 구인구직 리스트를 api를 통해 db에서 받아옴

--- lib/mainpage/widget/main_container.dart
+++ lib/mainpage/widget/main_container.dart
@@ -1,70 +1,86 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:jovajovajova/mainpage/widget/list_post_widget.dart';
-import 'package:jovajovajova/provider_class/addpost_provider.dart';
-import 'package:jovajovajova/provider_class/jobwrite_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:jovajovajova/model/jobvacancy_list_model.dart';
+import 'package:jovajovajova/provider_class/addpost_provider.dart';
+
+import 'list_post_widget.dart';
 
 class MainContainer extends StatelessWidget {
   final String title;
 
-  MainContainer({
+  const MainContainer({
     super.key,
     required this.title,
   });
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => JobWriteProvider(),
-      child: Flexible(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: const TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w700,
-                color: Colors.black
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w700,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey[100],
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                  ),
+                ],
+              ),
+              child: Consumer<AddpostProvider>(
+                builder: (context, provider, _) {
+                  return FutureBuilder(
+                    future: provider.fetchJobVacancies(),
+                    builder: (context, AsyncSnapshot<List<Jobvacancy>> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Center(
+                          child: Text("${snapshot.error}"),
+                        );
+                      } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                        final jobs = snapshot.data!;
+                        return ListView.separated(
+                          itemCount: jobs.length > 4 ? 4: jobs.length,
+                          itemBuilder: (context, index) {
+                            final job = jobs[index];
+                            return ListPostWidget(
+                              title: job.title,
+                              author: job.category,
+                              deadline: job.createdAt,
+                            );
+                          },
+                          separatorBuilder: (context, index) => const Divider(),
+                        );
+                      } else {
+                        return const Center(
+                          child: Text('No job vacancies available'),
+                        );
+                      }
+                    },
+                  );
+                },
               ),
             ),
-            const SizedBox(height: 20,),
-            Flexible(
-              child: Container(
-                padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey[100],
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 3,
-                      ),
-                    ]
-                  ),
-                  child: Consumer<AddpostProvider>(
-                    builder: (context,provider,_) {
-                      return  ListView.separated(
-                        physics: NeverScrollableScrollPhysics(),
-                        itemCount: 4,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ListPostWidget(
-                            title: provider.getTitleList()[index],
-                            author: provider.getAuthorList()[index],
-                            deadline: provider.getDeadlineList()[index],
-                          );
-                        },
-                        separatorBuilder: (BuildContext context, int index) {
-                          return const Divider();
-                        },
-                      );
-                    }
-                  ),
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
