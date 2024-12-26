e0eaba0a82fc1864f9dee7aff957a29cf891c2ef
LeeJueon <loolloo080313@gmail.com>
2024-12-26 20:59:42 +0900

    refactoring: api를 통해 db에서 값 가져오기

--- lib/jobpage/screen/job_screen.dart
+++ lib/jobpage/screen/job_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jovajovajova/main.dart';
+import 'package:jovajovajova/model/jobvacancy_list_model.dart';
 import 'package:jovajovajova/provider_class/addpost_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:jovajovajova/jobpage/screen/job_write_screen.dart';
@@ -10,51 +12,69 @@ class JobScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AddpostProvider(),
-      child: Scaffold(
-        appBar: const PreferredSize(
-          preferredSize: Size(200, 100),
-          child: MainAppbar(),
-        ),
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Consumer<AddpostProvider>(
-            builder: (context, provider, _) {
-              return ListView.separated(
-                itemCount: provider.getDayList().length,
-                itemBuilder: (BuildContext context, int index) {
-                  return JobListPost(
-                    day: provider.getDayList()[index]!,
-                    title: provider.getTitleList()[index],
-                    author: provider.getAuthorList()[index],
-                    deadline: provider.getDeadlineList()[index],
-                  );
+    return Scaffold(
+      appBar: MainAppbar(),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Consumer<AddpostProvider>(
+          builder: (context, provider, _) {
+            return FutureBuilder(
+                future: provider.fetchJobVacancies(),
+                builder: (context,AsyncSnapshot<List<Jobvacancy>> snapshot){
+                  if(snapshot.connectionState == ConnectionState.waiting){
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  else if(snapshot.hasError){
+                    return Center(
+                      child: Text("${snapshot.error}"),
+                    );
+                  }
+                  else if(snapshot.hasData && snapshot.data!.isNotEmpty){
+                    final jobs = snapshot.data!;
+                    return ListView.separated(
+                      itemCount: jobs.length,
+                      itemBuilder: (context,index) {
+                        final job = jobs[index];
+                        return JobListPost(
+                          day: 1,
+                          title: job.title,
+                          author: "이주언",
+                          deadline: job.createdAt,
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const Divider();
+                      },
+                    );
+                  }
+                  else{
+                    return Center(
+                      child: Text("값 없음"),
+                    );
+                  }
                 },
-                separatorBuilder: (BuildContext context, int index) {
-                  return const Divider();
-                },
-              );
-            },
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          shape: CircleBorder(),
-          backgroundColor: Colors.grey[400],
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => JobWriteScreen()),
             );
           },
-          child: const Icon(
-            Icons.edit,
-            color: Colors.white,
-          ),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       ),
+      floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
+        backgroundColor: Colors.grey[400],
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => JobWriteScreen()),
+          );
+        },
+        child: const Icon(
+          Icons.edit,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
