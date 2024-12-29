040eb4a45164b81e11da7703da149b736548b335
LeeJueon <loolloo080313@gmail.com>
2024-12-29 17:12:07 +0900

    feat: 버튼 클릭 가능 여부 조건문 추가

--- lib/myprofile_page/widget/information_modify_button.dart
+++ lib/myprofile_page/widget/information_modify_button.dart
@@ -3,34 +3,38 @@ import 'package:flutter/material.dart';
 
 class InformationModifyButton extends StatelessWidget {
   const InformationModifyButton({
-    super.key,
+    Key? key,
     required this.text,
     required this.color,
-    required this.onpressed});
+    required this.onpressed,
+  }) : super(key: key);
 
   final String text;
   final bool color;
-  final Function onpressed;
-
+  final VoidCallback? onpressed;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ButtonStyle(
         shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
         ),
-        backgroundColor: MaterialStateProperty.all(
-          color? Colors.grey[500]:Colors.white,
+        backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+              (Set<MaterialState> states) {
+            if (states.contains(MaterialState.disabled)) {
+              return Colors.grey[300];
+            }
+            return color ? Colors.grey[500] : Colors.white;
+          },
         ),
-
       ),
-      onPressed: () => onpressed,
+      onPressed: onpressed,
       child: Text(
         text,
         style: TextStyle(
-            color: color? Colors.white:Colors.grey[500],
-          fontWeight: FontWeight.bold
+          color: color ? Colors.white : Colors.grey[500],
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
