4c47f7e7bf999fab99a63bf5223e77f1ffbff461
LeeJueon <loolloo080313@gmail.com>
2024-12-29 17:10:58 +0900

    feat: 내 정보 상태관리 추가

--- lib/provider_class/myprofile_provider.dart
+++ lib/provider_class/myprofile_provider.dart
@@ -1,10 +1,40 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class MyProfile extends ChangeNotifier{
+class MyProfile extends ChangeNotifier {
+  String name = "이주언";
+  String github = "aiden30015";
+  String email = "s24068@gsm.hs.kr";
 
-  final name = "이주언";
-  final github = "aiden30015";
-  final email = "s24068@gsm.hs.kr";
+  // Temporary fields for editing
+  String tempName = "";
+  String tempGithub = "";
+  String tempEmail = "";
 
+  void updateName() {
+    if (tempName.isNotEmpty) {
+      name = tempName;
+    }
+    notifyListeners();
+  }
 
+  void updateGithub() {
+    if (tempGithub.isNotEmpty) {
+      github = tempGithub;
+    }
+    notifyListeners();
+  }
+
+  void updateEmail() {
+    if (tempEmail.isNotEmpty) {
+      email = tempEmail;
+    }
+    notifyListeners();
+  }
+
+  void backscreen(BuildContext context) {
+    tempName = "";
+    tempGithub = "";
+    tempEmail = "";
+    Navigator.pop(context);
+  }
 }
