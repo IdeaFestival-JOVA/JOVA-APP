b498de62cb8e00c8783737ce05d7f9f21abb10cb
LeeJueon <loolloo080313@gmail.com>
2024-12-29 17:11:32 +0900

    feat: 내 정보 변경

--- lib/myprofile_page/screen/myprofile_screen.dart
+++ lib/myprofile_page/screen/myprofile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jovajovajova/mainpage/widget/main_appbar.dart';
+import 'package:provider/provider.dart';
+import '../../provider_class/myprofile_provider.dart';
 import '../widget/myprofile_information.dart';
 
 class MyprofileScreen extends StatefulWidget {
@@ -13,29 +15,36 @@ class MyprofileScreen extends StatefulWidget {
 class _MyprofileScreenState extends State<MyprofileScreen> {
   @override
   Widget build(BuildContext context) {
+    final myprofileProvider = Provider.of<MyProfile>(context);
+
     return Scaffold(
       backgroundColor: Colors.white,
-        appBar: PreferredSize(
-          preferredSize: Size(200, 80),
-            child: MainAppbar()
-        ),
+      appBar: PreferredSize(
+        preferredSize: const Size(200, 80),
+        child: MainAppbar(),
+      ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "마이 페이지",
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 32,
-                ),
+          children: [
+            const Text(
+              "마이 페이지",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 32,
               ),
-              SizedBox(height: 20,),
-              MyprofileInformation(name: "이주언",github: "aiden30015",email: "s24068@gsm.hs.kr",),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+            MyprofileInformation(
+              name: myprofileProvider.name,
+              github: myprofileProvider.github,
+              email: myprofileProvider.email,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
